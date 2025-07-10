c82d7d0f010c1b564d9e30944028dfbb36e2b78d
MaheshNamala <namalamahesh44@gmail.com>
2025-07-10 06:51:11 +0530

    Update streaming_wordcount.py

--- streaming_wordcount.py
+++ streaming_wordcount.py
@@ -1,36 +1,33 @@
 from pyflink.datastream import StreamExecutionEnvironment
 from pyflink.table import StreamTableEnvironment, EnvironmentSettings
+from pyflink.table.expressions import col
 
 env = StreamExecutionEnvironment.get_execution_environment()
-settings = EnvironmentSettings.in_streaming_mode()
+settings = EnvironmentSettings.in_batch_mode()
 t_env = StreamTableEnvironment.create(env, environment_settings=settings)
 
-# Socket table
 t_env.execute_sql("""
     CREATE TABLE input_table (
         word STRING
     ) WITH (
-        'connector' = 'socket',
-        'hostname' = 'localhost',
-        'port' = '9999',
-        'format' = 'raw'
+        'connector' = 'filesystem',
+        'path' = 'input/stream_input.csv',
+        'format' = 'csv'
     )
 """)
 
-# Output to console
 t_env.execute_sql("""
     CREATE TABLE word_counts (
         word STRING,
-        count BIGINT
+        cnt BIGINT
     ) WITH (
         'connector' = 'print'
     )
 """)
 
-# Count logic
 t_env.execute_sql("""
     INSERT INTO word_counts
-    SELECT word, COUNT(*) as count
+    SELECT word, COUNT(*) AS cnt
     FROM input_table
     GROUP BY word
 """)
